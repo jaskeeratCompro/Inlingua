00dd3b5a3e1c20e20309610f6ff8033c6ac2b576
Jaskeerat Singh <jaskeerat.singh@comprotechnologies.com>
2024-07-11 13:14:01 +0530

    added iframe dimensions

--- script/eReader.js
+++ script/eReader.js
@@ -1,11 +1,28 @@
-window.onload = function(){
+window.onload = function () {
   const urlParams = new URLSearchParams(window.location.search);
-  const pageNo = urlParams.get('page');
+  const pageNo = urlParams.get("page");
   const url = `https://inlreader-dev.comprodls.com/demo/student-edition/interkulturel_1_vtext?page=${pageNo}`;
-  const iframe = document.createElement('iframe');
+  const iframe = document.createElement("iframe");
   iframe.src = url;
   iframe.allowFullscreen = true;
 
-  const iframeContainer = document.querySelector('#eReaderIframe');
+  const iframeContainer = document.querySelector("#eReaderIframe");
   iframeContainer.appendChild(iframe);
+
+  const sidebarContainer = document.querySelector(".sidebarContainer");
+  const sidebarDimensionContainer = document.querySelector(".sidebarDimensions");
+  const iframeDimensionContainer = document.querySelector(".iframeDimensions");
+
+  sidebarDimensionContainer.innerText = `Sidebar Dimensions: ${sidebarContainer.offsetWidth} x ${sidebarContainer.offsetHeight}`;
+  iframeDimensionContainer.innerText = `Iframe Dimensions: ${iframeContainer.offsetWidth} x ${iframeContainer.offsetHeight}`;
+};
+
+window.onresize = function () {
+  const iframeContainer = document.querySelector("#eReaderIframe");
+  const sidebarContainer = document.querySelector(".sidebarContainer");
+  const sidebarDimensionContainer = document.querySelector(".sidebarDimensions");
+  const iframeDimensionContainer = document.querySelector(".iframeDimensions");
+
+  sidebarDimensionContainer.innerText = `Sidebar Dimensions: ${sidebarContainer.offsetWidth} x ${sidebarContainer.offsetHeight}`;
+  iframeDimensionContainer.innerText = `Iframe Dimensions: ${iframeContainer.offsetWidth} x ${iframeContainer.offsetHeight}`;
 };
