6a56fd62ba3ff74ebf343ba7da59351b142b5ce7
Jaskeerat Singh <jaskeerat.singh@comprotechnologies.com>
2024-07-11 12:58:35 +0530

    added max-width to accordians

--- script/eReader.js
+++ script/eReader.js
@@ -1,9 +1,10 @@
-window.onload = function(){ debugger
+window.onload = function(){
   const urlParams = new URLSearchParams(window.location.search);
   const pageNo = urlParams.get('page');
   const url = `https://inlreader-dev.comprodls.com/demo/student-edition/interkulturel_1_vtext?page=${pageNo}`;
   const iframe = document.createElement('iframe');
   iframe.src = url;
+  iframe.allowFullscreen = true;
 
   const iframeContainer = document.querySelector('#eReaderIframe');
   iframeContainer.appendChild(iframe);
